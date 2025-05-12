f9b16e590e229833ff6834f15b70d341a2b75279
christian russo <christian.russo8@gmail.com>
2025-05-12 10:57:48 -0300

    dsada

--- src/app/cambiar-password/page.tsx
+++ src/app/cambiar-password/page.tsx
@@ -2,6 +2,10 @@
 
 import CambiarPasswordPage from '@/components/CambiarPasswordPage';
 
+// Evitar el prerenderizado estático
+export const dynamic = 'force-dynamic';
+// Alternativa: export const runtime = 'edge';
+
 export default function CambiarPassword() {
   return <CambiarPasswordPage />;
 }
